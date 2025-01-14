65e1e4861410915b83579ed32a8bfec98a5c6a88
Tiago S.C. <72054311+Tiago-Silverio-da-Costa@users.noreply.github.com>
2025-01-13 21:38:05 -0300

    feat: getAllPosts

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-use axum::{ extract::State, response::IntoResponse, routing::{get, post}, Json, Router};
+use axum::{
+    extract::State,
+    http::Method,
+    response::IntoResponse,
+    routing::{get, post},
+    Json, Router,
+};
+use chrono::{DateTime, Utc};
+use chrono::{NaiveDate, NaiveDateTime};
 use dotenv::dotenv;
+use serde::{Deserialize, Serialize};
 use sqlx::mysql::{MySqlPool, MySqlPoolOptions};
 use sqlx::FromRow;
-use serde::{Deserialize, Serialize};
-use tokio::net::TcpListener;
 use std::sync::Arc;
-use chrono::{NaiveDate, NaiveDateTime};
-use chrono::{DateTime, Utc};
+use tokio::net::TcpListener;
+use tower_http::cors::{Any, CorsLayer};
 
-#[derive(Debug, Serialize, Deserialize, FromRow)] 
+#[derive(Debug, Serialize, Deserialize, FromRow)]
 pub struct Post {
     id: Option<i32>,
     author_id: i32,
@@ -18,8 +25,8 @@ pub struct Post {
     category_id: i32,
     post_image_url: Option<String>,
     content: String,
-    created_at:  DateTime<Utc>,
-    updated_at: DateTime<Utc>
+    created_at: DateTime<Utc>,
+    updated_at: DateTime<Utc>,
 }
 
 #[derive(Debug, Serialize, Deserialize)]
@@ -35,11 +42,11 @@ pub struct Author {
     id: i32,
     name: String,
     bio: Option<String>,
-    photo_url: Option<String>
+    photo_url: Option<String>,
 }
 
 pub struct AppState {
-    db: MySqlPool
+    db: MySqlPool,
 }
 
 #[tokio::main]
@@ -48,7 +55,11 @@ async fn main() {
     println!("Starting Rest API Service");
 
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must set");
-    let pool = match MySqlPoolOptions::new().max_connections(10).connect(&database_url).await {
+    let pool = match MySqlPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+    {
         Ok(pool) => {
             println!("Connection to the database is successful!");
             pool
@@ -58,18 +69,34 @@ async fn main() {
             std::process::exit(1)
         }
     };
-    
+
+    let cors = CorsLayer::new()
+        .allow_origin(
+            "http://localhost:3000"
+                .parse::<axum::http::HeaderValue>()
+                .unwrap(),
+        )
+        .allow_methods([Method::GET, Method::POST, Method::PUT, Method::DELETE])
+        .allow_headers(Any);
+
     let app = Router::new()
         .route("/api/healthcheck", get(health_check_handle))
         .route("/api/posts", get(get_all_posts))
-        .route("/api/posts", post(create_post))
-        .route("/api/posts/{id}/comments", post(create_comment))
-        .with_state(Arc::new(AppState {db: pool.clone() }));
+        .route("/api/posts/{id}", get(get_post_by_id))
+        // .route("/api/login", post(auth_login))
+        // .route("/api/register", post(register))
+        // .route("/api/sendcodetoemail", post(send_code_to_email))
+        // .route("/api/checkcode", post(check_code))
+        // .route("/api/newpassword", post(create_new_password))
+        .with_state(Arc::new(AppState { db: pool.clone() }))
+        .layer(cors);
 
     println!("Server started succussfully at http://127.0.0.1:8080/api/healthcheck");
 
     let listener = TcpListener::bind("0.0.0.0:8080").await.unwrap();
-    axum::serve(listener, app.into_make_service()).await.unwrap();
+    axum::serve(listener, app.into_make_service())
+        .await
+        .unwrap();
 }
 
 pub async fn health_check_handle() -> impl IntoResponse {
@@ -92,43 +119,29 @@ pub async fn get_all_posts(State(state): axum::extract::State<Arc<AppState>>) ->
     Json(posts)
 }
 
-async fn create_post(
+pub async fn get_post_by_id(
     State(state): axum::extract::State<Arc<AppState>>,
-    Json(new_post): Json<Post>,
+    axum::extract::Path(id): axum::extract::Path<i32>,
 ) -> impl IntoResponse {
-    let result = sqlx::query!(
-        "INSERT INTO posts (title, description, content, post_image_url , author_id, category_id, publication_date, created_at, updated_at) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
-        new_post.title,
-        new_post.description,
-        new_post.content,
-        new_post.post_image_url,
-        new_post.author_id,
-        new_post.category_id,
-        new_post.publication_date,
-        new_post.created_at,
-        new_post.updated_at,
-    )
-    .execute(&state.db)
-    .await
-    .unwrap();
+    let result = sqlx::query_as::<_, Post>("SELECT * FROM posts WHERE id = ?")
+        .bind(id)
+        .fetch_optional(&state.db)
+        .await;
 
-    Json(serde_json::json!({ "status": "success", "affected_rows": result.rows_affected() }))
-
-}
-
-async fn create_comment(
-    State(state): axum::extract::State<Arc<AppState>>,
-    Json(new_comment): Json<Comment>,
-) -> impl IntoResponse {
-    let result = sqlx::query!(
-        "INSERT INTO comments (content, user_id, post_id) VALUES (?, ?, ?)",
-        new_comment.content,
-        new_comment.user_id,
-        new_comment.post_id
-    )
-    .execute(&state.db)
-    .await
-    .unwrap();
-
-    Json(serde_json::json!({ "status": "success", "affected_rows": result.rows_affected() }))
+    match result {
+        Ok(Some(post)) => Json(post).into_response(),
+        Ok(None) => (
+            axum::http::StatusCode::NOT_FOUND,
+            Json(serde_json::json!({ "error": "Post not found" })),
+        )
+            .into_response(),
+        Err(err) => {
+            eprintln!("Database query failed: {:?}", err);
+            (
+                axum::http::StatusCode::INTERNAL_SERVER_ERROR,
+                Json(serde_json::json!({ "error": "Internal Server Error" })),
+            )
+                .into_response()
+        }
+    }
 }
